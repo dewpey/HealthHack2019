2b847e9b122565efe3c4558b0f37ca28bd82e85f
Drew Patel <drew1patel@gmail.com>
2019-07-20 10:37:31 -0500

    did most of the UI

--- HealthHack2019/ViewController.swift
+++ HealthHack2019/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
         // Do any additional setup after loading the view.
     }
 
