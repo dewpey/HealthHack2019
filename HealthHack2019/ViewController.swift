e406df02e7a77069e469d28e58edfa5c39610ea8
Drew Patel <drew1patel@gmail.com>
2019-07-20 08:17:21 -0500

    Initial Commit

---
+++ HealthHack2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthHack2019
+//
+//  Created by Drew Patel on 7/20/19.
+//  Copyright © 2019 Drew Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
